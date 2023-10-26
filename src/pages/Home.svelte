4921a294c2483bf64d5c252af6ce3e0c58addf50
root <bobwatcherx@gmail.com>
2023-10-26 12:41:09 +0700

    321

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -7,9 +7,9 @@
 
  <div class="mt-3 ">  
 
-  <div style="display:flex;justify-content:space-between">
-  <Link to="/request" class="text-red"
-  style="text-decoration: none;"
+  <div style="display:flex;justify-content:space-evenly">
+  <Link to="/request" 
+  style="text-decoration: none;color:red;font-weight:bold"
   >Request Bokep Disini</Link>
   <a href="https://saweria.co/bobwatcherx"
   style="font-weight: bold;color: purple;" target="_blank"
--- src/pages/Request.svelte
+++ src/pages/Request.svelte
@@ -6,12 +6,12 @@
 	<Link to="/" class="btn btn-primary">Kembali</Link>
 
 	<div class="card shadow p-2 mt-2">
-		Selamat Ngewe boss..<span style="font-weight:bold">bang EWE</span>
-		<p>Kami akan selalu mengupdate bokep bokep bernutrisi 
-			setiap harinya . jadi harap pantengin web bokep ini .
-			kamu bisa request bokep kesayangan mu di bawah ini .
-
+		<p>Sayang Anak Sayang Bokep .
+		kami bandar bokep dan selaku panitia perbokep bokepan memberikan bokep teromontok terbohai setiap hari . jadi yang mau ngisi kode request . isi aja di bawah ini 
 		</p>
+
+	
 	</div>
-	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfq0OCwS8Z13pLGMQL3uf3EjGoIBPF9vbLXIWo0-rETlvZusQ/viewform?embedded=true" width="100%" height="407" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+	
+	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe-_La7SpyLj32Y2s2S8-MencIfRmYVSBiQAGbdUUmWT5BtmQ/viewform?embedded=true" width="100%" height="500px" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
 </div>
